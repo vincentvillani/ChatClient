caf08909e5c54f13e7492030de99f1b473e77f57
Vincent Villani <villanivincent@gmail.com>
2016-02-08 18:55:04 +1100

    The user can now type there own message into the chat box

--- UIFunctions.cpp
+++ UIFunctions.cpp
@@ -27,12 +27,7 @@ static void calculateRowsUsedByUIMessage(ClientData* clientData, UIMessage* mess
 	clientData->chatData.totalMessageRows += message->UIRowsUsed;
 }
 
-/*
-static const char* getMessageRowFromRowOffset(ClientData* clientData, int rowOffset, int drawableRows)
-{
 
-}
-*/
 
 
 //UI
@@ -72,7 +67,9 @@ void UISetup(ClientData* clientdata)
 		ss.str(""); //Clear the SS
 	}
 
-	clientdata->chatData.currentMessage = "Vincent:y8RkQ2m8PADX5H9qgxKy2twqCHGU1vCO0p3kmktJkpmQ43nkY8DeJ5mV8IgOCS4BHrjFp6EbU0npaO2yUnKu577hfYMcxZDSrDhMF44DbYspQIKrSShJ2BqSEUsEBEoNxDrUodZmwzqUH7CE4gvPuxNfKdWw hello";
+
+	//clientdata->chatData.currentMessage = "$|";
+	//clientdata->chatData.currentMessage = "Vincent:y8RkQ2m8PADX5H9qgxKy2twqCHGU1vCO0p3kmktJkpmQ43nkY8DeJ5mV8IgOCS4BHrjFp6EbU0npaO2yUnKu577hfYMcxZDSrDhMF44DbYspQIKrSShJ2BqSEUsEBEoNxDrUodZmwzqUH7CE4gvPuxNfKdWw hello";
 			//"ewANxmx3og5dJy8RkQ2m8PADX5H9qgxKy2twqCHGU1vCO0p3kmktJkpmQ43nkY8DeJ5mV8IgOCS4BHrjFp6EbU0npaO2yUnKu577hfYMcxZDSrDhMF44DbYspQIKrSShJ2BqSEUsEBEoNxDrUodZmwzqUH7CE4gvPuxNfKdWwOZuNE8Bxv023HAt3O5lWb6iV4KVHBjuYTYzB5PCnFnvd2Bk1ZuBrRWDA3ZJ2J124oM6fwCDal876sfoCFZOKOLiekruEIBHWjWnTHrRxk3BjRXCTEtKSSDIzsr0DxD2GRzyDM1AP3Xw97sl2Q2m1iGJQ2VazzpTCfxbl4PddT6u3wz4bSJUhdvlVYVR2naOmQwjxXODYWBlUkr8WQezAith0300rqJA55d1atGWyHzdiUPVYGZcP8oStQMGQe9aJHktGoSrMk5wqHxPI0gUmySfDTyncVHRMeOsMslXCopGEun4kpwdvUlTkpHGCbQjyRFYIqYILkTUcusFSXhQpeIH9PTbhYZDAYYzYeofoQc8y2CHHV7b1E7IU3wguwuZy0xuTRdkwiHVEQxa5F9nPm04msQdvHcU91uXLqqkEE82TTQeUcZ7YLSL3a8VQxhRFXXL1yYliUjO3zFxJDeOCpnYSS15Yzo6ZOVU3Sn5d1eWs6ZnVntkJE1VJ8jaCbuJZmnGhudUQ0OMwjumJw86v93OGi7CBZHnC7G6SwyNgIkPGMYIwcaPllAPiEOiNAuMSbY0TzYbrQEIl6vOmZS1HhbMG4jP5UnHfo2EAuuJMA9YqKdm7ltVf6SWMRwguznjHRTw9HegR5Jxd1BHN8n6wmkyg3vXSUmxfdm1JLAKgKrdlZiy7hCHZTA1Cqb44rfppAAWTWlUUKzzs4tbrPYMhUXSAfNzMzSJuu14RwkLIRDuKSOpEIpcjYRzb1xQw1kp6BngLNGQA9b4k6fJIV9kHpAHmWjq3XEs5bTiCgijLx8NSRVPzKx3kydqpiVogFl3gl1bvqFzHRnCw6v09kOpRBjZDWKOKS4yLsyNbqsHOeHncdYjbt9oB6so4S4P7fZbQVh1A4naDEJ3uaIplCWJ9HFzWqotZ5l7FZ0w1UCwLTXuaWmWfYIQGjXE2ReU4Fy8DcliR7tuJEpm5SQAKWcHaD5m6CfAxpL1mai2QPu1gVEHcPCk89FytrSNoYumWuiBbGiFo5WK6rE4iEoVn8Y5MjZd2etC91hbD5uPFeK7j3acT3ycGpQWlV7hO5frri98i1qHHGRb8Nd9CfAZ3Dec69d7RTH4efXbJHTaFMedQ1KJduikqNj88bpNb0MVz5W69nwdf2RnXprUqFZHEoEHJ25StTWxbqRBvWqkzHmPLGurfVwRDi0fx6g7YIkRK8hwqLVbwQUAzrM0qN7Qji938BgyO6P8gFDEhtGibFCIFSHJGnDORfeNndryevNSrAm712oTPZ6C34lnUlIrOGGx6g9twAtojos9ER798YdivDVcwoOVjrrpeyRpZeLhq1IulrcwcXJvOj2GxLIDPH1CR3VtY3EEwrGkAclzTpRkfJq18ScTSepf3EkSvxTaSLEfo51xarKtzO7sGz9vrj4ZeVhD5kjQq8t7jiQwyovu5vjKAxmXkWUPzxzgjdxXEAE1CJQ9U1e6wF8q5OvL7o2kEKhA9G7UsUpCzxFrWq3kIU8pMBtOZEQsdsaESPjAj2DOaJMewUpLBSUrsHJzH67QeIyX24KahWH8EPbAEX0HmfNiWD2c29zWQ8e5d04FWOzLI53IsrrDwZOkTyhmPOvFmy4Yfrw0bXgIbdrqS58aju2WXYeFko2u6HodModoa3hIpHXi4NH24DqaGHtmuIJmXTuheGNTO6HwVQ3NlBrbutAlpZKirRpHHwQmWGuOhfgP8RD25KjZtO0GP30QiZgWNvg7Z3uUBo8I8oxiQoi7x482pKMYne3ke78BV5FmshSh4hEePxjs500508HhxHb8Z9n7ekF1E5n8dntR69egijlNReRdLIfWrx2jEBYlzjiKRUDGhLrQSDQ9hKZHoacsvWVkJjVA3h5g7t11TIH0lwzYYSE5GZHBMQTxx9EzMf1JV5AmelvZ5SUYIIB08d60TEYKj7SrwZ9r6i2T601RSzTYfC26Xjci9YcbNVdMv2gOjOLVqC5ffdVl8Zx2SwgfFSCQpPvV39u9OjpTpPzW66o1WOq5w7bExS4CudQAg3tvdswbgCdNJugBhiiZulB3ugRi6ccjBK9cgEc4fzrt1gVJBPn39lwgIRXVcPZPFKDCb9J452ZGHwt9Rl5Gc4VdfATid4UvC7N3MS3Gmb7B3jCoBFvaey8aDxukgxXEIRVI6bbFA11gXZZLaEZuz6UN4o8A9njJXR7O4AelKfHMKiWYlbKyhZWV8aBYzxPzh3svpHaKKCc2Smns1BYkuoW5mZkMwLrfL5aA7BL6Phyhm3DCZZn3onR99ih4MhUWcvmuMFPFvKLc6HJC5E1pzFIKO4V610o5adM9pHSEepC8woXnmJLLKummIWdhfgpxqtjg15r7AIs46O1kL4DSGu5c0jzM1TaZBUZ47api5xk3UcFa4WOrTOfyGtcu6AwZxDyYNfH9PzvZDsjwXGPZtaayZa6R1fWBGbYHmN2IzkAJfhZdTVfXLaXk1WNXa9PHo3HLS03aw1PGhq7CIuBgoHBeJ5rqXSpMK4NFodMVIXPIC1n3qoiGU5Z1BsnSdD3m8bkm3NbwGVHAOv44J9fSqrOeetq7YssG4tHwnsGNSSzi5FbBfvxyuZiZlqGnA93T4qsUI89O8eOuHUUvBKtCIetqaSPi3WEUnQBdHf5fnlSL6rqAEEvvZ6KasvqPs0A03x0awggwOCxdMZEnakzaAXo0xeWS7Nbp97gb5R60Hoz8FL16zVuS7gvxXZUjIYUbHiLZI3ZsNg02wcdAK8EfFvLRXYij3RKMavIiOWeEs1A1gj8u1coqZby7QIWEtO9Ya8Fyu36YSmizdN5ch4ynHELLO";
 }
 
@@ -87,7 +84,9 @@ void UIUpdate(ClientData* clientdata)
 
 	int key = getch();
 
-	if(key == KEY_UP)
+	if(key == -1)
+		return;
+	else if(key == KEY_UP)
 	{
 		clientdata->chatData.baseViewableRow += 1;
 
@@ -105,6 +104,25 @@ void UIUpdate(ClientData* clientdata)
 		UIDraw(clientdata);
 
 	}
+	else if(key == KEY_ENTER || key == '\n')
+	{
+
+	}
+	else if(key == KEY_BACKSPACE) //This is a 'current message' character
+	{
+		if(clientdata->chatData.currentMessage.size())
+		{
+			//Delete the last character in the string
+			clientdata->chatData.currentMessage.erase(clientdata->chatData.currentMessage.end() - 1);
+			UIDraw(clientdata);
+		}
+	}
+	else
+	{
+		clientdata->chatData.currentMessage += key;
+		UIDraw(clientdata);
+	}
+
 
 
 }
@@ -124,6 +142,8 @@ void UIAddMessage(ClientData* clientdata, UIMessage* newMessage)
 
 void UIDraw(ClientData* clientdata)
 {
+	//const std::string currentMessagePrefix = "$|";
+
 	//Clear the window
 	clear();
 
@@ -279,6 +299,9 @@ void UIDraw(ClientData* clientdata)
 		drawingRowsUsed += 1;
 
 	}
+
+	//Move the curser to the char past the last one in the current string message
+	move(cd->maxRow - 1, (cd->currentMessage.size() % cd->maxCol));
 
 
 	refresh(); //Actually draw it to the window
